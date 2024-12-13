a766a2329abf09c19f69f91ae1a9e35ecfb107c4
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-13 20:04:01 +0900

    🎉 initial first commit

---
+++ src/components/providers.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toast } from '@/components/ui'
+import { useRouter } from 'next/navigation'
+import type { ReactNode } from 'react'
+import { RouterProvider } from 'react-aria-components'
+
+declare module 'react-aria-components' {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>['push']>[1]
+    >
+  }
+}
+
+export function Providers({ children }: { children: ReactNode }) {
+  const router = useRouter()
+
+  return (
+    <RouterProvider navigate={router.push}>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Toast />
+        {children}
+      </ThemeProvider>
+    </RouterProvider>
+  )
+}
