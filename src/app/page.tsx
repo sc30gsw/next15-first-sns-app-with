bef4592b94d49457052523552c46f6197fd5d81f
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-14 18:45:11 +0900

    ✨ session取得処理作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-const Home = () => {
-  return <div>hello</div>
+import { getSession } from '@/libs/auth/session'
+
+const Home = async () => {
+  const session = await getSession()
+  return <div>hello {session?.user?.id}</div>
 }
 
 export default Home
