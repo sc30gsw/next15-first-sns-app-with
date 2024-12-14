6e39e2e6b1ca2e428e9d2fe842861327d9a1e85a
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-14 19:23:39 +0900

    💄 各UI作成

---
+++ src/features/likes/components/like-button.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { Form, TextField } from '@/components/ui'
+import { likeAction } from '@/features/likes/actions/like-action'
+import { likeFormSchema } from '@/features/likes/types/schema/like-form-schema'
+import { getFormProps, getInputProps, useForm } from '@conform-to/react'
+import { getZodConstraint, parseWithZod } from '@conform-to/zod'
+import { IconHeart, IconHeartFill } from 'justd-icons'
+import { useSession } from 'next-auth/react'
+import { useActionState, useOptimistic } from 'react'
+
+type LikeButtonProps = {
+  postId: string
+  initialLikes: string[]
+}
+
+export const LikeButton = ({ postId, initialLikes }: LikeButtonProps) => {
+  const { data: session } = useSession()
+
+  const [optimisticLike, setOptimisticLike] = useOptimistic<
+    { likeCount: number; isLiked: boolean },
+    void
+  >(
+    {
+      likeCount: initialLikes.length,
+      isLiked: session?.user?.id
+        ? initialLikes.includes(session.user.id)
+        : false,
+    },
+    (currentState) => ({
+      likeCount: currentState.isLiked
+        ? currentState.likeCount - 1
+        : currentState.likeCount + 1,
+      isLiked: !currentState.isLiked,
+    }),
+  )
+
+  const [lastResult, action] = useActionState(likeAction, null)
+
+  const [form, fields] = useForm({
+    constraint: getZodConstraint(likeFormSchema),
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: likeFormSchema })
+    },
+    onSubmit() {
+      setOptimisticLike()
+    },
+    defaultValue: {
+      postId,
+    },
+  })
+
+  return (
+    <Form
+      {...getFormProps(form)}
+      action={action}
+      className="flex items-center gap-1"
+    >
+      <TextField {...getInputProps(fields.postId, { type: 'hidden' })} />
+      <button type="submit" className="flex items-center gap-2">
+        {optimisticLike.isLiked ? (
+          <IconHeartFill className="size-5 text-red-500" />
+        ) : (
+          <IconHeart className="size-5" />
+        )}
+      </button>
+      <span className={optimisticLike.isLiked ? 'text-red-500' : ''}>
+        {optimisticLike.likeCount}
+      </span>
+    </Form>
+  )
+}
---
+++ src/features/posts/components/post-edit-modal.tsx
@@ -0,0 +1,98 @@
+import { Button, Form, Loader, Modal, TextField } from '@/components/ui'
+import { editPost } from '@/features/posts/actions/edit-post-action'
+import { postFormSchema } from '@/features/posts/types/schema/post-form-schema'
+import { getFormProps, getInputProps, useForm } from '@conform-to/react'
+import { getZodConstraint, parseWithZod } from '@conform-to/zod'
+import { IconSend } from 'justd-icons'
+import { useRouter } from 'next/navigation'
+import { useActionState, useEffect } from 'react'
+import { toast } from 'sonner'
+
+type PostEditModalProps = {
+  postId: string
+  content: string
+  isOpen: boolean
+  onClose: () => void
+}
+
+export const PostEditModal = ({
+  postId,
+  content,
+  isOpen,
+  onClose,
+}: PostEditModalProps) => {
+  const [lastResult, action, isPending] = useActionState(editPost, null)
+
+  const [form, fields] = useForm({
+    constraint: getZodConstraint(postFormSchema),
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: postFormSchema })
+    },
+
+    defaultValue: {
+      content,
+    },
+  })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (lastResult?.status === 'success') {
+      toast('Successfully updated on your post!', {
+        action: {
+          label: 'View',
+          onClick: () => {
+            router.push(`/posts/${postId}`)
+          },
+        },
+      })
+    }
+  }, [lastResult, postId, router])
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={isPending ? undefined : onClose}>
+      <Modal.Content>
+        <Modal.Header>
+          <Modal.Title>Edit Post</Modal.Title>
+        </Modal.Header>
+        <Form
+          {...getFormProps(form)}
+          action={(formData) => {
+            formData.append('postId', postId)
+            action(formData)
+          }}
+        >
+          <TextField
+            {...getInputProps(fields.content, { type: 'text' })}
+            placeholder="What's on your mind?"
+            isDisabled={isPending}
+            defaultValue={
+              lastResult?.initialValue?.content.toString() ?? content
+            }
+            className="w-full"
+            errorMessage={''}
+          />
+          <span
+            id={fields.content.errorId}
+            className="mt-1 text-sm text-red-500"
+          >
+            {fields.content.errors}
+          </span>
+          <Modal.Footer>
+            <Modal.Close isDisabled={isPending}>Cancel</Modal.Close>
+            <Button
+              type="submit"
+              size="square-petite"
+              appearance="outline"
+              className={'h-10'}
+              isDisabled={isPending || fields.content.value === undefined}
+            >
+              {isPending ? <Loader /> : <IconSend />}
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal.Content>
+    </Modal>
+  )
+}
